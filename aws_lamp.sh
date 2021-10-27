11a816ce95c606781fea1ff09c50f14088ab5a43
jayssj11 <cooljay.gupta@gmail.com>
2021-10-27 11:35:48 +0200

    Update aws_lamp.sh

--- aws_lamp.sh
+++ aws_lamp.sh
@@ -2,7 +2,6 @@ sudo apt-get update
 sudo apt-get install apache2
 sudo apt-get install php php-mysql
 sudo apt-get install libapache2-mod-php
-sudo apt-get install phpmyadmin
 sudo apt-get install mysql-server
 sudo apt-get install mysql-client-core-5.7
-
+sudo apt-get install phpmyadmin
